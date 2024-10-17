cae726dd7b9840fe408268a6842d48d849b48bc3
kakhramanovruslan <kahramanovruslan085@gmail.com>
2024-10-17 12:00:41 +0500

    create application

---
+++ src/test/java/com/project/career_hub/CareerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.career_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
